f703006325469cf91a5a3d8c0587795e92edcfa9
Stanislav Kazakov <d34zhas@gmail.com>
2022-09-01 09:06:02 +0300

    Added exercise 1.1

---
+++ ch01/ex01/main.c
@@ -0,0 +1,15 @@
+/********************************************************************
+ * Author:  Stanislav Kazakov <d34zhas@gmail.com>
+ * Date:    09.01.2022
+ * ==============================================
+ * Exercise 1-1.Run the "hello, world" program on your system.
+ * Experiment with leaving out parts of the program, to see what
+ * error messages you get.
+ ********************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World");
+}

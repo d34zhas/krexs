681f90e28a5c644991d6cacbef896942e71ee928
Stanislav Kazakov <d34zhas@gmail.com>
2022-09-07 07:55:07 +0300

    Added excercise 1.7

---
+++ ch01/ex07/main.c
@@ -0,0 +1,14 @@
+/********************************************************************
+ * @Author:  Stanislav Kazakov <d34zhas@gmail.com>
+ * @Date:    09.07.2022
+ * ==============================================
+ * Exercise 1-7:
+ * Write a program to print the value of EOF.
+ ********************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("EOF: %d\n", EOF);
+}
